c60afec34be12456a1e4984ea372619794d07f1b
lutfir2019 <lutfirrahman2019@gmail.com>
2024-09-04 15:09:53 +0700

    happy code

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
       <header>
         <nav>
           <ul>
-            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/blog">Blog</Link></li>
           </ul>
